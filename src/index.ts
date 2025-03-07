2be6fb9e46380c332ca998acfc871fe172e160bb
rudinesurya <rudinesurya@gmail.com>
2025-03-07 16:32:54 +0000

    update

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './application-create-response.interface';
-export * from './application-delete-response.interface';
-export * from './application-search-response.interface';
-export * from './application-update-response.interface';
-export * from './application-update.interface';
-export * from './application.interface';
-export * from './applications-search-response.interface';
